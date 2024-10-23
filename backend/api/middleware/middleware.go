916aac395224380a13bfcea1da6df6559bf73439
PixelDev0506 <90635777+PixelDev0506@users.noreply.github.com>
2024-10-23 20:07:21 +0200

    Delete backend directory

--- backend/api/middleware/middleware.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "github.com/aaantiii/lostapp/backend/api/services"
-
-var authService services.IAuthService
-
-func UseAuthService(auth services.IAuthService) {
-	authService = auth
-}
--- backend/api/types/discord.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// DiscordUser is used to store the user data from the Discord API and rename the JSON struct tags.
-type DiscordUser struct {
-	ID        string   `json:"id"`
-	Name      string   `json:"name"`
-	AvatarURL string   `json:"avatarUrl"`
-	Roles     []string `json:"-"`
-}
